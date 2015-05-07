bcb262e0449d4d2d38455a10ed80acb69bb865c9
Ranjith Kumar <ranji2612@gmail.com>
2015-05-08 03:13:41 +0530

    changes for openshift

--- config/database.js
+++ config/database.js
@@ -1,6 +1,6 @@
 module.exports = {
 	// the database url to connect
 	//url : 'mongodb://ranji2612:war10ck_dev@kahana.mongohq.com:10063/app28446989'
-	url : 'mongodb://localhost:27017/poll'
+	url : 'mongodb://admin:H5pPzClcCc_u@localhost:27017/poll'
 	
 }
